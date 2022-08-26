c1930ff9200f0ccd27eba611645a059faf117547
Marius <marius.gheorghies@protonmail.com>
2022-08-26 18:58:15 +0200

    use gihub for release download

--- scripts/04-install-questdb.sh
+++ scripts/04-install-questdb.sh
@@ -6,7 +6,8 @@ mkdir /tmp/questdb
 cloud-init status --wait
 
 # Download and extract QuestDB
-curl -s "https://dl.questdb.io/snapshots/questdb-${QUESTDB_VERSION}-no-jre-bin.tar.gz" -o /tmp/questdb.tar.gz
+echo "Downloading QuestDB version ${QUESTDB_VERSION}"
+curl -sL "https://github.com/questdb/questdb/releases/download/${QUESTDB_VERSION}/questdb-${QUESTDB_VERSION}-no-jre-bin.tar.gz" -o /tmp/questdb.tar.gz
 tar xf /tmp/questdb.tar.gz -C /tmp/questdb --strip-components 1
 mv /tmp/questdb/questdb.jar /usr/local/bin/
 
